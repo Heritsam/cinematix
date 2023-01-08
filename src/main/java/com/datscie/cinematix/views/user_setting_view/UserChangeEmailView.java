5f2d6cc82bf6335751e985b8abda49600d289a97
akmalmzkki <akmalmzkki@student.telkomuniversity.ac.id>
2023-01-08 17:55:20 +0700

    kk

--- src/main/java/com/datscie/cinematix/views/user_setting_view/UserChangeEmailView.java
+++ src/main/java/com/datscie/cinematix/views/user_setting_view/UserChangeEmailView.java
@@ -48,6 +48,11 @@ public class UserChangeEmailView extends javax.swing.JFrame {
         jLabel2.setText("Password Confirmation");
 
         jButton1.setText("Edit");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Cancel");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -110,6 +115,10 @@ public class UserChangeEmailView extends javax.swing.JFrame {
         // TODO add your handling code here:
         this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
