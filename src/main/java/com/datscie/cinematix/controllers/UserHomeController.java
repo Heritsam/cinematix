d22a65809f99c718ef12ef405f72c20a9ecd2aa2
Quin Derbi <quinderbi@gmail.com>
2023-01-09 22:28:16 +0700

    Menambah sesuatu quin

--- src/main/java/com/datscie/cinematix/controllers/UserHomeController.java
+++ src/main/java/com/datscie/cinematix/controllers/UserHomeController.java
@@ -7,16 +7,20 @@ package com.datscie.cinematix.controllers;
 
 import com.datscie.cinematix.dao.MovieDao;
 import com.datscie.cinematix.dao.MovieDaoImpl;
+import com.datscie.cinematix.dao.ScheduleDao;
+import com.datscie.cinematix.dao.ScheduleDaoImpl;
 import com.datscie.cinematix.models.Movie;
+import com.datscie.cinematix.models.Schedule;
 import com.datscie.cinematix.utils.ApplicationException;
+
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.logging.*;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JLabel;
 import javax.swing.JList;
-
-
+import javax.swing.JTextPane;
 
 /**
  *
@@ -24,9 +28,11 @@ import javax.swing.JList;
  */
 public class UserHomeController {
     MovieDao movieDao;
+    ScheduleDao scheduleDao;
 
     public UserHomeController() {
         movieDao = new MovieDaoImpl();
+        scheduleDao = new ScheduleDaoImpl();
     }
 
     public void fecthMovies(JList<String> jListMovies) {
@@ -42,17 +48,27 @@ public class UserHomeController {
         }
     }
 
-    public void displayMovieDetails(String selectedTitle ,JLabel jLabelMovieTitle, JLabel jLabelMovieGenre, JLabel jLabelMovieDirector,JLabel jLabelDuration) {
+    public void displayMovieDetails(String selectedTitle, JLabel jLabelMovieTitle, JLabel jLabelMovieGenre,
+            JLabel jLabelMovieDirector, JLabel jLabelDuration, JTextPane jLabelSynopsis, JList<String> jList2) {
         // TODO: Implement this method
         try {
             Movie movie = movieDao.getMoviesByTitle(selectedTitle);
-            jLabelMovieTitle.setText("Title   : "+movie.getTitle());
-            jLabelMovieGenre.setText("Genre   : "+movie.getGenre());
-            jLabelMovieDirector.setText("Director: "+movie.getDirector());
-            jLabelDuration.setText("Duration: "+movie.getDuration());
+            ArrayList<Schedule> schedules = scheduleDao.getScheduleByMovieId(movie.getId());
+            jLabelMovieTitle.setText("Title   : " + movie.getTitle());
+            jLabelMovieGenre.setText("Genre   : " + movie.getGenre());
+            jLabelMovieDirector.setText("Director: " + movie.getDirector());
+            jLabelDuration.setText("Duration: " + movie.getDuration());
+            jLabelSynopsis.setText("Synopsis: " + movie.getSynopsis());
+
+            DefaultListModel<String> model = new DefaultListModel<>();
+            schedules.forEach((schedule) -> {
+                model.addElement(schedule.getDateTimeString());
+            });
+            jList2.setModel(model);
+
         } catch (ApplicationException ex) {
             Logger.getLogger(UserHomeController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
 }
--- src/main/java/com/datscie/cinematix/dao/ScheduleDao.java
+++ src/main/java/com/datscie/cinematix/dao/ScheduleDao.java
@@ -7,7 +7,12 @@ import com.datscie.cinematix.utils.ApplicationException;
 
 public interface ScheduleDao {
     public ArrayList<Schedule> getAllSchedule() throws ApplicationException;
+
     public void addSchedule(Schedule schedule) throws ApplicationException;
+
     public void updateSchedule(Schedule schedule) throws ApplicationException;
+
     public void deleteSchedule(Schedule schedule) throws ApplicationException;
+
+    public ArrayList<Schedule> getScheduleByMovieId(int movieId) throws ApplicationException;
 }
